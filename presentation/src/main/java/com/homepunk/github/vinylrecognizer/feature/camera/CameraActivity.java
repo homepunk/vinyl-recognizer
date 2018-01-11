1b37b005b000e8900e8583b13fecfc6090b25b97
homepunk/work <nikita.storozhenko@gmail.com>
2018-01-11 15:33:13 +0200

    made activity full screen, no preview stretching

--- presentation/src/main/java/com/homepunk/github/vinylrecognizer/feature/camera/CameraActivity.java
+++ presentation/src/main/java/com/homepunk/github/vinylrecognizer/feature/camera/CameraActivity.java
@@ -19,6 +19,7 @@ import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -63,8 +64,6 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_camera);
         surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
         cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
@@ -109,6 +108,20 @@ public class CameraActivity extends AppCompatActivity {
         closeCamera();
         stopCameraBackgroundThread();
         super.onPause();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocas) {
+        super.onWindowFocusChanged(hasFocas);
+        View decorView = getWindow().getDecorView();
+        if(hasFocas) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
     }
 
     @SuppressLint({"NewApi", "MissingPermission"})
